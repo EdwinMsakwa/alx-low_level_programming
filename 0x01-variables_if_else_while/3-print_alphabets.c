44905d1b279d2843d9dc80f46932e8ba0298a8bb
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-03-19 08:45:40 +0200

    Commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - main entry point to the program.
+ * Description: Prints the alphabet characters.
+ * Return: returns 0 if execution is successful, 1 otherwise.
+ */
+
+int main(void)
+{
+	int lowercaseStartAsciiCode = 97;
+	int lowercaseStopAsciiCode = 122;
+
+	while (lowercaseStartAsciiCode <= lowercaseStopAsciiCode)
+	{
+		putchar(lowercaseStartAsciiCode);
+		lowercaseStartAsciiCode += 1;
+	}
+
+	int uppercaseStartAsciiCode = 65;
+	int uppercaseStopAsciiCode = 90;
+
+	while (uppercaseStartAsciiCode <= uppercaseStopAsciiCode)
+	{
+		putchar(uppercaseStartAsciiCode);
+		uppercaseStartAsciiCode += 1;
+	}
+
+	putchar(10);
+
+	return (0);
+}
