585ab4694db90c4933687ec7f3266838839471bf
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-03-21 07:54:10 +0200

    Commit

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - prints alphabetic charatecers.
+ * Description: The program outputs alphabetic characters to
+ * the standard output.
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int letterAsciiCode;
+
+	for (letterAsciiCode = 97 ; letterAsciiCode <= 122; letterAsciiCode++ )
+	{
+		_putchar(letterAsciiCode);
+	}
+	 _putchar('\n');
+
+}
