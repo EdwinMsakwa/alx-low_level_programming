b30df7925fb706a7484e1cbd743da179958c3c0d
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-03-21 07:46:06 +0200

    Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+void print_alphabet(void);
+
+/**
+ * main - Main entry point to the program
+ * Description: The program prints the string _putchar to the standard output.
+ * Return: returns 0 upon success , otherwise 1.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
@@ -7,7 +11,25 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-
 int _putchar(char c);
 
+/**
+ * print_alphabet - prints alphabetic charatecers.
+ * Description: The program outputs alphabetic characters to
+ * the standard output.
+ * Return: void
+ */
 
+void print_alphabet(void)
+{
+	int letterAsciiCode;
+
+	for (letterAsciiCode = 97 ; letterAsciiCode <= 122; letterAsciiCode++)
+	{
+		_putchar(letterAsciiCode);
+	}
+	 _putchar('\n');
+
+}
+
+#endif /* _MAIN_H_ */
