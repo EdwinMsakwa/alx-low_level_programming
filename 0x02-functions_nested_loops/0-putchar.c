477e3d8477f859d2f45d78e0386ad2c954c5a40f
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-03-21 06:58:19 +0200

    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include "main.h"
-
+#include "_putchar.c"
 /**
  * main - Main entry point to the program.
  * Description: The program prints the string _putchar to the standard output.
