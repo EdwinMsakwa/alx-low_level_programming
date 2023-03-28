98eda211b7edbd47e236df61e718215acc44d86e
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-03-28 04:39:16 +0200

    Commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,18 +11,18 @@ void rev_string(char *s)
 {
 	char temp;
 	int len = 0, i, j;
-	
+
 	while (s[len] != '\0')
-		{
-			len++;
-		}
-    
-	for (i = 0, j = len - 1; i < j; i++, j--) 
+	{
+		len++;
+	}
+
+	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
 		s[j] = temp;
 	}
-}	
+}
 
 
