ed821bdf330e80a3ac446549f0b31f683caf3f3e
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-04-12 05:27:25 +0200

    Commit message

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * create_array - Creates an array of chars.
+  * @size: Size of the array
+  * @c: The char to fill in the array
+  *
+  * Return: Returns a pointer to the array, or NULL if it fails
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * free_grid - frees a 2 dimensional grid
+  * @grid: grid argument
+  * @height: height argument
+  *
+  * Return: void
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
+
