438e772705351e62a183854f2819119431f3a7cd
Edwin Msakwa <edwinmsakwa@yahoo.com>
2023-04-20 05:13:40 +0200

    Assignment Commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
   * main - prints opcodes
   * @argc: argument
   * @argv: argument lis
   * Return: 0 success
   */
+
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-
-	int val = 0, i = 0;
-
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		return (1);
 	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	unsigned char *code_ptr = (unsigned char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", *(code_ptr + i));
+	}
+	printf("\n");
 
 	return (0);
 }
